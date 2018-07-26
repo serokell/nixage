2f913157cb7c073b56b30129e61bdf63dd82569e
Kirill Elagin <kirelagin@gmail.com>
2018-07-26 23:39:25 +0300

    Pick up changes from the rebased version

--- hs/cli/Main.hs
+++ hs/cli/Main.hs
@@ -14,8 +14,8 @@ import Types
 import Parser
 
 main :: IO ()
-main = execParser (info nixageP infoMod) >>= \case
-    StackCmd args -> stackAction args
+main = execParser (info (helper <*> nixageP) infoMod) >>= \case
+    StackCmd stackArgs     -> stackAction stackArgs
     ConvertCmd convertArgs -> convertAction convertArgs
   where
     infoMod = header "Nixage"
@@ -25,13 +25,12 @@ main = execParser (info nixageP infoMod) >>= \case
 
 -- | * Nixage command actions
 
--- | read ProjectYaml from file
+-- | read ProjectYaml from project.yaml
 readProjectYaml :: (MonadIO m, MonadThrow m) => FilePath -> m ProjectYaml
 readProjectYaml projectYamlPath =
     liftIO (decodeFileEither projectYamlPath) >>= \case
       Left err -> throwM $ YamlDecodingError (show err)
       Right projectYaml -> return projectYaml
-
 
 -- | Write stack and snapshot yaml files
 writeStackConfig :: (MonadIO m, MonadThrow m)
@@ -49,17 +48,19 @@ writeStackConfig stackPath snapshotPath stackConfig = do
 -- and './nixage-stack[rand-num].yaml', and run 'stack' on them.
 stackAction :: (MonadIO m, MonadThrow m, MonadMask m) => StackArgs-> m ()
 stackAction args = do
-    projectYaml <- readProjectYaml "project.yaml"
-    let projectNative = projectYamlToProjectNative projectYaml
-    let stackConfig = projectNativeToStackConfig projectNative
-    withSystemTempFile "nixage-stack-snapshot.yaml" $ \snapshotPath _ ->
-      withTempFile "." "nixage-stack.yaml" $ \stackPath _ -> do
-        writeStackConfig stackPath snapshotPath stackConfig
-        let  args' = ["--stack-yaml", toText stackPath] <> args
-        liftIO $ do
-            (_,_,_,handle) <- createProcess $
-                (proc "stack" (toString <$> args')) { delegate_ctlc = True }
-            void $ waitForProcess handle
+    liftIO (decodeFileEither "project.yaml") >>= \case
+      Left err -> throwM $ YamlDecodingError (show err)
+      Right projectYaml -> do
+        let projectNative = projectYamlToProjectNative projectYaml
+        let stackConfig = projectNativeToStackConfig projectNative
+        withSystemTempFile "nixage-stack-snapshot.yaml" $ \snapshotPath _ ->
+          withTempFile "." "nixage-stack.yaml" $ \stackPath _ -> do
+            writeStackConfig stackPath snapshotPath stackConfig
+            let  args' = ["--stack-yaml", toText stackPath] <> args
+            liftIO $ do
+                (_,_,_,handle) <- createProcess $
+                     (proc "stack" (toString <$> args')) { delegate_ctlc = True }
+                void $ waitForProcess handle
 
 -- | Conversion between project specification formats.
 convertAction :: (MonadIO m, MonadThrow m) => ConvertArgs -> m ()
